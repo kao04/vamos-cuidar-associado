7554e10c101e15219d9b08565996e0b6e0207a4a
Valsantana <valsantana@gmail.com>
2025-12-03 18:19:29 -0300

    deixando pronto

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// kao04/vamos-cuidar-associado/vamos-cuidar-associado-c2b45ae6ed14aac04028d1e4f00207d2dbdd5a7e/src/App.tsx
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -13,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/vamoscuidarassociado">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
